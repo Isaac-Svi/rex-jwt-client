3111b18e6c6c461795ddc534ccc1ba58b580bd19
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-04-18 19:26:24 +0300

    update privatelink to use class as well

--- src/components/Links/PrivateLink.js
+++ src/components/Links/PrivateLink.js
@@ -2,15 +2,37 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import AuthProvider from '../../providers/AuthProvider'
 
-const PrivateLink = ({ to, children }) => {
-  const context = AuthProvider.contextType
-  return (
-    <context.Consumer>
-      {({ accessToken, loading }) => {
-        return accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
-      }}
-    </context.Consumer>
-  )
+// const PrivateLink = ({ to, children }) => {
+//   const context = AuthProvider.contextType
+//   return (
+//     <context.Consumer>
+//       {({ accessToken, loading }) => {
+//         return accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
+//       }}
+//     </context.Consumer>
+//   )
+// }
+
+const context = AuthProvider.contextType
+
+class PrivateLink extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <context.Consumer>
+        {({ accessToken, loading }) => {
+          return accessToken && !loading ? (
+            <Link to={this.props.to}>{this.props.children}</Link>
+          ) : (
+            <></>
+          )
+        }}
+      </context.Consumer>
+    )
+  }
 }
 
 export default PrivateLink
--- src/components/Links/PublicLink.js
+++ src/components/Links/PublicLink.js
@@ -2,17 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthProvider from '../../providers/AuthProvider'
 
-// const PublicLink = ({ to, children }) => {
-//   const context = AuthProvider.contextType
-//   return (
-//     <context.Consumer>
-//       {({ accessToken, loading }) => {
-//         return !accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
-//       }}
-//     </context.Consumer>
-//   )
-// }
-
 const context = AuthProvider.contextType
 
 class PublicLink extends Component {
