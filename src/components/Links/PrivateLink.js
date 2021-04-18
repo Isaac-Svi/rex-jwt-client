2ef14fcc1ae027d7d36e7bd8b77fa9c1d69e3a53
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-04-18 19:56:48 +0300

    remove comments in privatelink and add component

--- src/components/Links/PrivateLink.js
+++ src/components/Links/PrivateLink.js
@@ -1,17 +1,6 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthProvider from '../../providers/AuthProvider'
-
-// const PrivateLink = ({ to, children }) => {
-//   const context = AuthProvider.contextType
-//   return (
-//     <context.Consumer>
-//       {({ accessToken, loading }) => {
-//         return accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
-//       }}
-//     </context.Consumer>
-//   )
-// }
 
 const context = AuthProvider.contextType
 
