12dbdcb4b295ce66357921bce494fb5c630638f7
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-04-18 17:55:08 +0300

    change PublicLink to class component to increase compatability with other libraries

--- src/components/Links/PublicLink.js
+++ src/components/Links/PublicLink.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import AuthProvider from '../../providers/AuthProvider'
 
-const PublicLink = ({ to, children }) => {
-  const context = AuthProvider.contextType
-  return (
-    <context.Consumer>
-      {({ accessToken, loading }) => {
-        return !accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
-      }}
-    </context.Consumer>
-  )
+// const PublicLink = ({ to, children }) => {
+//   const context = AuthProvider.contextType
+//   return (
+//     <context.Consumer>
+//       {({ accessToken, loading }) => {
+//         return !accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
+//       }}
+//     </context.Consumer>
+//   )
+// }
+
+const context = AuthProvider.contextType
+
+class PublicLink extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <context.Consumer>
+        {({ accessToken, loading }) => {
+          return !accessToken && !loading ? <Link to={to}>{children}</Link> : <></>
+        }}
+      </context.Consumer>
+    )
+  }
 }
 
 export default PublicLink
