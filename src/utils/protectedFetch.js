e45b4c55b2dbeb34e83d1c2ff98956838e5d877b
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-04-22 15:36:41 +0300

    revert to original protectedFetch

--- src/utils/protectedFetch.js
+++ src/utils/protectedFetch.js
@@ -1,36 +1,46 @@
-import { useContext } from 'react'
-import AuthProvider from '../providers/AuthProvider'
 import isExp from './isExp'
 import parseToType from './parseToType'
 
-const { setRefresh } = useContext(AuthProvider.contextType)
-
 const protectedFetch = async ({
-  accessToken,
+  accessToken: token,
   body = {},
   headers = {},
   method,
+  refreshMethod,
   route,
+  refreshRoute,
   responseType = 'json',
 }) => {
   try {
-    if (isExp(accessToken)) {
-      setRefresh()
+    let res, data
+
+    if (isExp(token)) {
+      res = await fetch(refreshRoute, {
+        method: refreshMethod,
+        headers: {
+          credentials: 'include',
+        },
+      })
+      const { accessToken, error } = await res.json()
+
+      if (error) throw new Error(error)
+
+      token = accessToken
     }
 
     const params = {
       method,
       headers: {
-        Authorization: `Bearer ${accessToken}`,
+        Authorization: `Bearer ${token}`,
         ...headers,
       },
     }
     if (method.toLowerCase() !== 'get') {
-      params.body = body
+      params['body'] = body
     }
 
-    const res = await fetch(route, params)
-    const data = await parseToType(res, responseType)
+    res = await fetch(route, params)
+    data = await parseToType(res, responseType)
 
     return { data, error: '' }
   } catch (err) {
@@ -38,52 +48,5 @@ const protectedFetch = async ({
     return { data: '', error: err.message }
   }
 }
-// const protectedFetch = async ({
-//   accessToken: token,
-//   body = {},
-//   headers = {},
-//   method,
-//   refreshMethod,
-//   route,
-//   refreshRoute,
-//   responseType = 'json',
-// }) => {
-//   try {
-//     let res, data
-
-//     if (isExp(token)) {
-//       res = await fetch(refreshRoute, {
-//         method: refreshMethod,
-//         headers: {
-//           credentials: 'include',
-//         },
-//       })
-//       const { accessToken, error } = await res.json()
-
-//       if (error) throw new Error(error)
-
-//       token = accessToken
-//     }
-
-//     const params = {
-//       method,
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//         ...headers,
-//       },
-//     }
-//     if (method.toLowerCase() !== 'get') {
-//       params['body'] = body
-//     }
-
-//     res = await fetch(route, params)
-//     data = await parseToType(res, responseType)
-
-//     return { data, error: '' }
-//   } catch (err) {
-//     console.log(err.message)
-//     return { data: '', error: err.message }
-//   }
-// }
 
 export default protectedFetch
