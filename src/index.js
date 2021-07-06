951b30eeb5a6cf4610bfda20a6792679b425c2c6
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-06 17:23:44 +0300

    add isExpired function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import AuthProvider from './providers/AuthProvider'
 import useProtectedFetch from './hooks/useProtectedFetch'
 import protectedFetch from './utils/protectedFetch'
+import isExpired from './utils/isExp'
 import { PublicRoute, PrivateRoute } from './components/Routes'
 import { PublicLink, PrivateLink } from './components/Links'
 
@@ -8,6 +9,7 @@ export {
   AuthProvider,
   useProtectedFetch,
   protectedFetch,
+  isExpired,
   PublicRoute,
   PrivateRoute,
   PublicLink,
