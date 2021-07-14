370e159b648d61dcf26ac2a300bd38c65c51033e
Isaac Svi <yitzchaksviridyuk@gmail.com>
2021-07-14 13:05:56 +0300

    allow redirect to accept object as well

--- src/components/Routes/PrivateRoute.js
+++ src/components/Routes/PrivateRoute.js
@@ -3,22 +3,31 @@ import { Route, Redirect } from 'react-router-dom'
 import AuthProvider from '../../providers/AuthProvider'
 
 const PrivateRoute = (props) => {
-  const { redirect, component: Component, ...rest } = props
-  const context = AuthProvider.contextType
-  return (
-    <context.Consumer>
-      {({ accessToken, loading, loader: Loader }) => {
-        if (!accessToken && loading)
-          return Loader ? <Loader /> : <div>loading...</div>
+    const { redirect, component: Component, ...rest } = props
+    const context = AuthProvider.contextType
+    return (
+        <context.Consumer>
+            {({ accessToken, loading, loader: Loader }) => {
+                if (!accessToken && loading)
+                    return Loader ? <Loader /> : <div>loading...</div>
 
-        return accessToken ? (
-          <Route {...rest} component={Component} />
-        ) : (
-          <Redirect to={redirect} />
-        )
-      }}
-    </context.Consumer>
-  )
+                return accessToken ? (
+                    <Route {...rest} component={Component} />
+                ) : (
+                    <Redirect
+                        to={
+                            typeof redirect === 'object'
+                                ? {
+                                      pathname: redirect.pathname,
+                                      ...redirect,
+                                  }
+                                : redirect
+                        }
+                    />
+                )
+            }}
+        </context.Consumer>
+    )
 }
 
 export default PrivateRoute
